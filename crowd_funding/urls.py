3c39ddc3779bfd54cc56f03b9ee2ac33478a7258
ahmed emad <ahmed.emad2051@gmail.com>
2020-03-21 19:14:07 +0200

    add users, projects, main apps with base template

--- crowd_funding/urls.py
+++ crowd_funding/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth.views import LoginView 
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('logIn', LoginView.as_view(template_name='logIn.html')),
+    path('auth/', include("authenticate.urls")),
+    path('users/', include("users.urls")),
+    path('projects/', include("projects.urls")),
+    path('home/', include("main.urls")),
 ]
