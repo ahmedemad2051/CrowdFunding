f9ec21edaa751d378c30fe331984f7309a937e33
ahmed emad <ahmed.emad2051@gmail.com>
2020-03-22 14:44:57 +0200

    add project create & view page

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from projects import views
 urlpatterns = [
-    path('',views.index)
+    path('',views.index, name="projects.all"),
+    path('<int:project_id>',views.show, name="projects.show"),
+    path('create',views.create, name="projects.create"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(req):
     return render(req, "projects/index.html")
+
+
+def show(req, project_id):
+    return render(req, "projects/show.html")
+
+
+def create(req):
+    return render(req, "projects/create.html")
