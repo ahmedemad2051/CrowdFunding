e635e7e51cc75b235d17c5028973e822de8c62f7
ahmed emad <ahmed.emad2051@gmail.com>
2020-03-29 19:10:51 +0200

    make owner nullable

--- projects/models.py
+++ projects/models.py
@@ -10,13 +10,15 @@ class Project(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, null=True)
     desc = models.TextField(max_length=1500)
-    owner = models.ForeignKey(to="users.Account", on_delete=models.CASCADE)
+    owner = models.ForeignKey(to="users.Account", on_delete=models.CASCADE, null=True)
     category = models.ForeignKey(to="Category", on_delete=models.CASCADE)
     tags = TaggableManager()
     total = models.DecimalField(decimal_places=2, max_digits=10)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+
 
     def __str__(self):
         return self.title
