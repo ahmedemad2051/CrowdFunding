7593fd4853c30d631645563dbb6f98315455f3e1
mo7amedsalah <mohamed.s.abokhadra@gmail.com>
2020-03-30 22:50:19 +0200

    Retrieve 5 Latest Projects from database

--- main/views.py
+++ main/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
 from projects.models import Project
-from django.shortcuts import render, get_object_or_404, redirect
-from projects.forms import ProjectForm, ImageForm
-from django.http import HttpResponse
-from django.forms import modelformset_factory
-from projects.models import Image, Project
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from django.http import HttpResponseRedirect
-from django.db.models import Sum
-from decimal import Decimal
 
 # Create your views here.
 
 def index(req):
     selectedProjects = Project.objects.filter(active=True) [:6]
-    latestProjects = Project.objects.order_by('-created_at') 
+    latestProjects = Project.objects.order_by('-created_at') [:5] 
 
 
     context={
