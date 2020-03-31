d130b8308a9700bfc30daa4735f1dbc92200365c
Jaxon <mohamedmostels@gmail.com>
2020-03-31 21:31:05 +0200

    working profile projects section

--- users/views.py
+++ users/views.py
@@ -3,12 +3,10 @@ from django.contrib.auth.decorators import login_required
 from projects.models import Image, Project, Comment
 
 # Create your views here.
-def profile(req):
-    return render(req, "users/profile.html")
-
 @login_required
-def display_user_projects(request):
+def profile(request):
     context = {}
     user_projects = Project.objects.filter(owner = request.user) # get user projects
     context['user_projects'] = user_projects
-    return render(request, "users/profile", context)
+    return render(request, "users/profile.html", context)
+
