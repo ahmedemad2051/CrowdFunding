5f5980a70005fabfaaedd9427543345697af9aef
Jaxon <mohamedmostels@gmail.com>
2020-03-31 20:36:42 +0200

    Profile updated

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from projects.models import Image, Project, Comment
 
 # Create your views here.
 def profile(req):
@@ -7,6 +9,6 @@ def profile(req):
 @login_required
 def display_user_projects(request):
     context = {}
-    user_projects = project.objects.filter(owner = request.user) # get user projects
+    user_projects = Project.objects.filter(owner = request.user) # get user projects
     context['user_projects'] = user_projects
     return render(request, "users/profile", context)
