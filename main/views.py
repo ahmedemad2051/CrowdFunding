aade325085bacbc2b26216d6315cde28926532b8
mo7amedsalah <mohamed.s.abokhadra@gmail.com>
2020-03-31 19:46:34 +0200

    remove comment

--- main/views.py
+++ main/views.py
@@ -21,8 +21,8 @@ def contact(request):
     return render(request, "main/contact.html")
 
 
-def search(request):
-    query = request.GET.get('q')
-    results = Project.objects.filter(Q(title__icontains=query) | Q(tags__icontains=query))
-
-    return render(request, "projects/index.html", {"results": results})
+# def search(request):
+#     query = request.GET.get('q')
+#     results = Project.objects.filter(Q(title__icontains=query) | Q(tags__icontains=query))
+#
+#     return render(request, "projects/index.html", {"results": results})
