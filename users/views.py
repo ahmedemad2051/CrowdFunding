822b1f1c03ce18968cf47c662952e46a9e3977e6
mo7amedsalah <mohamed.s.abokhadra@gmail.com>
2020-04-02 15:23:31 +0200

    edit profile

--- users/views.py
+++ users/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from projects.models import Image, Project, Comment
+from projects.models import Image, Project, Comment, Donation
+
 
 # Create your views here.
 @login_required
 def profile(request):
     context = {}
     user_projects = Project.objects.filter(owner = request.user) # get user projects
+    # user_donations = Donation.objects.filter(project = request.user)
+    # print(user_donations)
     context['user_projects'] = user_projects
+    # context['user_donations'] = user_donations
+
     return render(request, "users/profile.html", context)
 
