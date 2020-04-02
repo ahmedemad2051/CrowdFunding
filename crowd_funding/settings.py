9c0bd2a80ce2a868516339f84baaaa65e0a314fc
mo7amedsalah <mohamed.s.abokhadra@gmail.com>
2020-04-02 21:53:11 +0200

    edit profile page

--- crowd_funding/settings.py
+++ crowd_funding/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'crowd_funding',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'Root123#',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
